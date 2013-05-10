cfbc4eb876e85b12f16728fbd1f3c9d0b96481d6
cloudnull <kevin@bkintegration.com>
2013-05-09 21:41:13 -0500

    now if a fabric attack vector is chosen, the attack is randomly chosen as well as additional accompanying attack vectors.

--- psycorax/attacks/fabricvector.py
+++ psycorax/attacks/fabricvector.py
@@ -13,34 +13,48 @@ class Scrapper(object):
                                "dd if=/dev/urandom of=/dev/null"
                                " bs=1024000 count[100*1000] &; done"),
                'dd_the_root': ("dd if=/dev/urandom of=$(mount -l |"
-                               " awk '/on \/\ / {print $1}')"
-                               " bs=10240 count=10000 &"),
-                'network_offline': ("for i in `ifconfig | awk '/eth/ {print $1}"
-                                    "'`; do ip l s down $i;done &"),
-                'rm_rm_slash': ("rm -rf --one-file-system --no-preserve-root"
-                                " / &"),
-                'stop_things': ("if [ -f '/etc/init.d/apache' ]; then"
-                                " /etc/init.d/apache stop;"
-                                "elif [ -f '/etc/init.d/apache2' ]; then"
-                                " /etc/init.d/apache2 stop;"
-                                "elif [ -f '/etc/init.d/httpd' ]; then"
-                                " /etc/init.d/httpd stop;"
-                                "fi"
-                                "if [ -f '/etc/init.d/mysql' ]; then"
-                                " /etc/init.d/mysql stop;"
-                                "elif [ -f '/etc/init.d/mysqld' ]; then"
-                                " /etc/init.d/mysqld stop;"
-                                "fi"
-                                "if [ -f '/etc/init.d/nginx' ]; then"
-                                " /etc/init.d/nginx stop;"
-                                "fi")}
+                               " awk '/on \/\ / {print $1}') &"),
+               'network_offline': ("for i in `ifconfig | awk '/eth/ {print $1}"
+                                   "'`; do ip l s down $i;done &"),
+               'rm_rf_slash': ("rm -rf --one-file-system --no-preserve-root"
+                               " / &"),
+               'stop_things': ("if [ -f '/etc/init.d/apache' ]; then"
+                               " /etc/init.d/apache stop;"
+                               "elif [ -f '/etc/init.d/apache2' ]; then"
+                               " /etc/init.d/apache2 stop;"
+                               "elif [ -f '/etc/init.d/httpd' ]; then"
+                               " /etc/init.d/httpd stop;"
+                               "fi"
+                               "if [ -f '/etc/init.d/mysql' ]; then"
+                               " /etc/init.d/mysql stop;"
+                               "elif [ -f '/etc/init.d/mysqld' ]; then"
+                               " /etc/init.d/mysqld stop;"
+                               "fi"
+                               "if [ -f '/etc/init.d/nginx' ]; then"
+                               " /etc/init.d/nginx stop;"
+                               "fi")}
         self.vector = _av
 
     def fab_data(self):
-        self.attack_name, self.attack = random.choice(self.vector.items())
-        return self.attack_name
+        """
+        Pick a few attacks and create the naming string
+        """
+        name = []
+        self.attack = []
+        all_attack = self.vector.items()
+        for _ in range(random.randint(1, len(self.vector.keys()))):
+            attack_name, attack = random.choice(all_attack)
+            name.append(attack_name)
+            self.attack.append(attack)
+            index = all_attack.index((attack_name, attack))
+            all_attack.pop(index)
+        joinname = ' '.join(name)
+        return joinname
 
     def fab_settings(self, instance):
+        """
+        Set the fabric Settings for instance interactions
+        """
         for addr in instance['addresses']['public']:
             if addr['version'] == 4:
                 ip_addr = addr['addr']
@@ -61,6 +75,10 @@ class Scrapper(object):
         Run an attack with the idea that we are going to break some things
         """
         self.fab_settings(instance)
-        with self.settings:
-            run(self.attack)
-        disconnect_all()
+        if self.attack:
+            with self.settings:
+                for attack in self.attack:
+                    run(attack)
+            disconnect_all()
+        else:
+            self.output.info('Nothing chosen for attacking')
